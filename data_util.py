c87fb9f2a2beb68d1026c9030adf2a94d204b073
Trung Nguyen <trung.chi.nguyen.97@gmail.com>
2019-11-09 15:14:17 -0800

    Clean up the data utility

--- data_util.py
+++ data_util.py
@@ -4,24 +4,15 @@ from urllib.request import urlopen
 from zipfile import ZipFile
 
 if __name__ == '__main__':
-    if os.path.isdir("data") and os.path.isfile("data/connect-4.dat"):
+    if os.path.isdir("data") and os.path.isfile("data/connect-4.data"):
         print("Data has already been downloaded.")
     else:
         print("Downloading data...")
 
         # For data description, see
-        url = "https://sci2s.ugr.es/keel/dataset/data/classification/connect-4.zip"
+        url = "https://drive.google.com/uc?export=download&id=1B3wRs75_mFrSZbAJQFIZIxt6G1jTKMzm"
         resp = urlopen(url)
         with ZipFile(BytesIO(resp.read())) as zipfile:
             zipfile.extractall("data")
-
-        # Cleaning the data format
-        with open("data/connect-4.dat", "r") as f:
-            lines = [line for line in f if line[0] is not "@"]
-
-        with open("data/connect-4.dat", "w") as f:
-            f.writelines(line.replace("win", "1")
-                         .replace("loss", "-1")
-                         .replace("draw", "0") for line in lines)
 
         print("Finished downloading data")
